ad7b8094f02e703e36dc773461ffad04eef6ebc3
VeerLH <zinlynnhtun7979@gmail.com>
2025-01-04 20:52:58 +0900

    first commit

--- src/components/home/EmojiBullet.js
+++ src/components/home/EmojiBullet.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import {Box} from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 
 function EmojiBullet(props) {
-    const {emoji, text} = props;
+  const { emoji, text } = props;
 
-    return (
-        <Box component={'li'} fontSize={'1rem'} lineHeight={1.5} style={{cursor: 'default'}}>
-            <Box component={'span'} aria-label="cheese"
-                 role="img"
-                 mr={{xs: '0.5rem', md: '1rem'}} fontSize={'1.5rem'}>{emoji}</Box> {text}
-        </Box>
-    );
+  return (
+    <Box
+      component={"li"}
+      fontSize={"1rem"}
+      lineHeight={1.5}
+      style={{ cursor: "default" }}
+    >
+      <Box
+        component={"span"}
+        aria-label="cheese"
+        role="img"
+        mr={{ xs: "0.5rem", md: "1rem" }}
+        fontSize={"1.5rem"}
+        color="#000"
+      >
+        {emoji}
+      </Box>{" "}
+      {text}
+    </Box>
+  );
 }
 
 export default EmojiBullet;
